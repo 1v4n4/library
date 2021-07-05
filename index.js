dfdad4c60b329e323139a4331721bec219360040
ja <ivananl@yahoo.com>
2021-07-05 20:43:17 +0200

    Add readme

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const bookForm = document.getElementById('bookForm');
 const tbody = document.querySelector('tbody');
-const table = document.querySelector("table");
-let myLibrary = []
+const table = document.querySelector('table');
+
+const myLibrary = [];
 
 function Book(title, author, pages) {
   this.title = title;
@@ -11,7 +12,7 @@ function Book(title, author, pages) {
 }
 
 function bookToLibrary(book) {
-  myLibrary.push(book)
+  myLibrary.push(book);
 }
 
 function bookToDom(book) {
@@ -20,27 +21,28 @@ function bookToDom(book) {
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.pages}</td>
-      <td class="reads">${book.read}</td>
+      <td class='reads'>${book.read}</td>
       <td><button class="changeStatusBtn btn btn-secondary">Change status</button></td>
       <td><button class="deleteBtn btn btn-danger">Delete book</button></td>
-    </tr>  
+    </tr>
   `;
 }
 
 function deleteBook(arr, value) {
-  var a = []
-  for(var i = 0; i<arr.length; i++) {
-    if(arr[i].title == value) {
-      a = arr.splice(i, 1)
+  let a = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i].title === value) {
+      a = arr.splice(i, 1);
     }
   }
   return a;
 }
 
 function changeStatus(arr, value) {
-  for(var i = 0; i<arr.length; i++) {
-    if(arr[i].title == value) {
-      arr[i].read == true ? arr[i].read = false : arr[i].read = true;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i].title === value) {
+      // eslint-disable-next-line
+      arr[i].read === true ? arr[i].read = false : arr[i].read = true;
     }
   }
 }
@@ -48,102 +50,62 @@ function changeStatus(arr, value) {
 function clearInput(form) {
   form[0].value = '';
   form[1].value = '';
-  form[2].value= '';
+  form[2].value = '';
 }
 
+// eslint-disable-next-line
 function bookCheck(title, author, arr) {
-  for(var i = 0; i<arr.length; i++) {
-    if(arr[i].title == title && arr[i].author == author) {
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i].title === title && arr[i].author === author) {
       return true;
     }
   }
 }
 
-bookForm.addEventListener("submit", (e) => {
-  e.preventDefault()
-
-console.log("Form submitted")
+// eslint-disable-next-line
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
   const newTitle = bookForm[0].value;
   const newAuthor = bookForm[1].value;
   const newPages = bookForm[2].value;
-  
+
   let confirmation = true;
 
-  if (!newTitle) {
-    alert("You have to fill all data");
-    confirmation = false;
-  } 
-
-  if (!newAuthor) {
-    alert("You have to fill all data");
+  if (!newTitle || !newAuthor || !newPages) {
+    alert('You have to fill all data');
     confirmation = false;
   }
 
-  if (!newPages) {
-    alert("You have to fill all data");
+  if (bookCheck(newTitle, newAuthor, myLibrary)) {
+    alert('Book with same title by same author already exists');
     confirmation = false;
   }
 
-  if (bookCheck(newTitle, newAuthor, myLibrary) == true) {
-    alert("Book with same title by same author already exists");
-    confirmation = false;
-  }
-
-  if (confirmation == false) {
-    console.log(myLibrary)
+  if (confirmation === false) {
     return false;
-      }
-
-  else {
-  
-    const newBook = new Book(newTitle, newAuthor, newPages);
-    bookToLibrary(newBook);
-    bookToDom(newBook);
-    console.log(newBook);
-    console.log(myLibrary) 
-    clearInput(bookForm)
   }
+
+  const newBook = new Book(newTitle, newAuthor, newPages);
+  bookToLibrary(newBook);
+  bookToDom(newBook);
+  clearInput(bookForm);
 });
 
-
-
-table.addEventListener("click", (e) => {
-  e.preventDefault()
+table.addEventListener('click', (e) => {
+  e.preventDefault();
 
   const btn = e.target;
-    var removeData = btn.closest("tr").firstElementChild.textContent;
-    console.log(removeData)
+  const removeData = btn.closest('tr').firstElementChild.textContent;
 
-  if (btn.classList.contains("deleteBtn")) {
-      
-
-    deleteBook(myLibrary, removeData)
-
-    console.log(myLibrary)
-
-    let removed = btn.closest("tr").remove();
-    console.log(removed)
-    console.log("Book deleted from DOM");
-    console.log(btn)
-
+  if (btn.classList.contains('deleteBtn')) {
+    deleteBook(myLibrary, removeData);
+    btn.closest('tr').remove();
+  } else
+  if (btn.classList.contains('changeStatusBtn')) {
+    changeStatus(myLibrary, removeData);
+    const changed = btn.parentElement.previousElementSibling;
+    // eslint-disable-next-line
+      changed.textContent === 'false' ? changed.innerHTML = 'true' : changed.innerHTML = 'false';
   }
-  else 
-    if (btn.classList.contains("changeStatusBtn")) {
-      console.log("Status changed");
-
-      changeStatus(myLibrary, removeData)
-      console.log(myLibrary)
-
-      let changed = btn.parentElement.previousElementSibling;
-      console.log(changed)
-
-      changed.textContent == 'false' ? changed.innerHTML = 'true' : changed.innerHTML = 'false'
-    }
-
 });
-
-/*function deleteBook {
-
-
-} */
